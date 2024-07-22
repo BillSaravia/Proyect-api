b430c3f8b8c1ac80eb81564777da58d5d21748a0
Bill Saravia <bill.saravia@tecsup.edu.pe>
2024-07-21 22:37:16 -0500

    cambio

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ import categoriesRouter from "../routes/categoriesRouter.js";
 import brandsRouter from "../routes/brandsRouter.js";
 import colorRouter from "../routes/colorRouter.js";
 import reviewRouter from "../routes/reviewRouter.js";
-
+import orderRouter from "../routes/ordersRouter.js";
 
 
 //db connect
@@ -25,6 +25,8 @@ app.use("/api/v1/categories/", categoriesRouter);
 app.use("/api/v1/brands/", brandsRouter);
 app.use("/api/v1/colors/", colorRouter);
 app.use("/api/v1/reviews/", reviewRouter);
+app.use("/api/v1/orders/", orderRouter);
+
 //err middleware
 app.use(notFound);
 app.use(globalErrhandler);
